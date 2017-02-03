dec6660b00c8b9664e9cdafd45d75c144661cbe6
Terry Kang <rkdxogns1234@daum.net>
2017-02-03 14:28:21 -0800

    closes #6

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+                        
+$route['dunno'] = function() {
+	$source = '../data/anonymous.jpg'; // an image you provide, outside of "oublic"!
+	// set the mime type for that image (jpeg, png, etc)
+	header("Content-type: image/jpeg"); 
+	header('Content-Disposition: inline');
+	readfile($source); // dish it
+	die(); // and we don't have to go any further
+};        
